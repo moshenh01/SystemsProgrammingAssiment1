0f2b1041796b1d562eed82e0e5702caa4fa9c996
Your Namemoshenh01exit[200~git config --global user.name Your <moshenh01@gmail.com>
2022-11-23 13:13:14 +0200

    add3

--- main.c
+++ main.c
@@ -6,41 +6,41 @@ int main() {
     
     while((scanf("%d  %d",&start,&end) != 2));
     if( (start > 0) && (end > 0)){
-        printf("The Armstrong numbers are: ");
+        printf("The Armstrong numbers are:");
         int count1 = 0;
         for(int i = start;i < end;i++){
             if(isArmstrong(i) == 1){
-            printf("%d ", i);
+            printf(" %d", i);
             count1++;
             if(count1 % 26 == 0)
                 printf("\n");
             }
         }
-        printf("\nThe Palindromes are: ");
+        printf("\nThe Palindromes are:");
         int count2 = 0;
         for(int i = start;i < end;i++){
             if(isPalindrome(i) == 1){
-            printf("%d ",i);
+            printf(" %d",i);
             count2++;
             if(count2 % 26 == 0)
                 printf("\n");
             }
         }
-            printf("\nThe Prime numbers are: ");
+            printf("\nThe Prime numbers are:");
         int count3 = 0;
         for(int i = start;i < end;i++) {
             if(isPrime(i) == 1){
-            printf("%d ",i);
+            printf(" %d",i);
             count3++;
             if (count3 % 26 == 0)
                 printf("\n");
                 }
             }
-            printf("\nThe Strong numbers are: ");
+            printf("\nThe Strong numbers are:");
             int count4 = 0;
             for (int i = start; i < end; i++) {
                 if(isStrong(i) == 1){
-                printf("%d ",i);
+                printf(" %d",i);
                 count4++;
                 if (count4 % 26 == 0)
                     printf("\n");
