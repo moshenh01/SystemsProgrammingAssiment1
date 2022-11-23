4eabe2c8e3ba41e770d1511583e7746d8d558cc1
Your Namemoshenh01exit[200~git config --global user.name Your <moshenh01@gmail.com>
2022-11-23 13:22:35 +0200

    add5

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-// this is main
+// this is main 
 #include "NumClass.h"
 
 int main() {
@@ -14,22 +14,19 @@ int main() {
             } 
         }
         printf("\nThe Palindromes are:");
-        
+
         for(int i = start;i < end;i++){
             if(isPalindrome(i) == 1){
             printf(" %d",i);
             }
         }
             printf("\nThe Prime numbers are:");
-        
         for(int i = start;i < end;i++) {
             if(isPrime(i) == 1){
             printf(" %d",i);
             }
-           
         }
             printf("\nThe Strong numbers are:");
-           
             for (int i = start; i < end; i++) {
                 if(isStrong(i) == 1){
                 printf(" %d",i);
